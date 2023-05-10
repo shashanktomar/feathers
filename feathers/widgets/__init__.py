439639332dcbf5175ae4fb26bae8e5e7ef52411b
Shashank Tomar <shashank004@gmail.com>
2023-05-10 20:35:51 +1000

    temp commit

--- feathers/widgets/__init__.py
+++ feathers/widgets/__init__.py
@@ -1,7 +1,8 @@
+from ._cached_view import CachedView
 from ._cursor_view import CursorView
 from ._input import Input
 from ._text_area import TextArea
 from ._text_view import TextView
 from .help import Help, HelpEntry, HelpProvider
 
-__all__ = ["Help", "HelpEntry", "HelpProvider", "Input", "TextArea", "CursorView", "TextView"]
+__all__ = ["Help", "HelpEntry", "HelpProvider", "Input", "TextArea", "CursorView", "TextView", "CachedView"]
---
+++ feathers/widgets/_cached_view.py
@@ -0,0 +1,156 @@
+from __future__ import annotations
+
+from typing import Self, cast
+
+from rich.console import RenderableType
+from rich.highlighter import ReprHighlighter
+from rich.pretty import Pretty
+from rich.protocol import is_renderable
+from rich.text import Text
+from textual.geometry import Size
+from textual.scroll_view import ScrollView
+from textual.strip import Strip
+
+from feathers.cache import CacheListener, RenderablesCache, RenderableWithOptions
+
+
+class CachedView(ScrollView, CacheListener):
+    DEFAULT_CSS = """
+    TextLog{
+        background: $surface;
+        color: $text;
+        overflow-y: scroll;
+    }
+    """
+
+    wrap: bool = False
+    highlight: bool = False
+    markup: bool = False
+    auto_scroll: bool = True
+
+    def __init__(
+        self,
+        *,
+        wrap: bool = False,
+        highlight: bool = False,
+        markup: bool = False,
+        auto_scroll: bool = True,
+        name: str | None = None,
+        id: str | None = None,
+        classes: str | None = None,
+        disabled: bool = False,
+    ) -> None:
+        """Create a TextLog widget.
+
+        Args:
+            max_lines: Maximum number of lines in the log or `None` for no maximum.
+            min_width: Minimum width of renderables.
+            wrap: Enable word wrapping (default is off).
+            highlight: Automatically highlight content.
+            markup: Apply Rich console markup.
+            auto_scroll: Enable automatic scrolling to end.
+            name: The name of the text log.
+            id: The ID of the text log in the DOM.
+            classes: The CSS classes of the text log.
+            disabled: Whether the text log is disabled or not.
+        """
+        super().__init__(name=name, id=id, classes=classes, disabled=disabled)
+        self.wrap = wrap
+        """Enable word wrapping."""
+        self.highlight = highlight
+        """Automatically highlight content."""
+        self.markup = markup
+        """Apply Rich console markup."""
+        self.auto_scroll = auto_scroll
+        """Automatically scroll to the end on write."""
+        self.highlighter = ReprHighlighter()
+
+        self._renderables_cache: RenderablesCache = RenderablesCache(self.app.console, listener=self)
+
+    def notify_style_update(self) -> None:
+        self._renderables_cache.refresh()
+
+    def _extract_renderable(
+        self, content: RenderableType | object, width: int | None, expand: bool | None, shrink: bool | None
+    ) -> RenderableWithOptions:
+        renderable: RenderableType
+        if not is_renderable(content):
+            renderable = Pretty(content)
+        else:
+            if isinstance(content, str):
+                if self.markup:
+                    renderable = Text.from_markup(content)
+                else:
+                    renderable = Text(content)
+                if self.highlighter is not None:
+                    renderable = self.highlighter(renderable)
+            else:
+                renderable = cast(RenderableType, content)
+
+        return RenderableWithOptions(renderable, width, expand, shrink, self.wrap)
+
+    def write(
+        self,
+        content: RenderableType | object,
+        width: int | None = None,
+        expand: bool = False,
+        shrink: bool = True,
+        scroll_end: bool | None = None,
+    ) -> Self:
+        """Write text or a rich renderable.
+
+        Args:
+            content: Rich renderable (or text).
+            width: Width to render or `None` to use optimal width. Only used if either or both expand and shrink are True.
+            expand: Enable expand to widget width, or `False` to use `width`.
+            shrink: Enable shrinking of content to fit width.
+            scroll_end: Enable automatic scroll to end, or `None` to use `self.auto_scroll`.
+
+        Returns:
+            The `TextLog` instance.
+        """
+
+        renderable = self._extract_renderable(content, width, expand, shrink)
+        self._renderables_cache.add(renderable)
+
+        auto_scroll = self.auto_scroll if scroll_end is None else scroll_end
+        if auto_scroll:
+            self.scroll_end(animate=False)
+
+        return self
+
+    def on_resize(self):
+        self._renderables_cache.content_width = self.scrollable_content_region.width
+
+    def clear(self) -> Self:
+        self._renderables_cache.clear()
+        self.max_width = 0
+        self.virtual_size = Size(0, 0)
+        self.refresh()
+        return self
+
+    def render_line(self, y: int) -> Strip:
+        scroll_x, scroll_y = self.scroll_offset
+        line = self._render_line(scroll_y + y, scroll_x, self.size.width)
+        strip = line.apply_style(self.rich_style)
+        return strip
+
+    def on_cache_update(self):
+        self.virtual_size = self._renderables_cache.virtual_size
+        if self.auto_scroll:
+            self.scroll_end(animate=False)
+
+    def _render_line(self, y: int, scroll_x: int, width: int) -> Strip:
+        strip = self._renderables_cache.strip_at(y)
+        if strip is None:
+            return Strip.blank(width, self.rich_style)
+
+        # key = (y + self._start_line, scroll_x, width, self.max_width)
+        # if key in self._line_cache:
+        #     return self._line_cache[key]
+
+        # line = strip.adjust_cell_length(max(self.max_width, width), self.rich_style).crop(scroll_x, scroll_x + width)
+        line = strip.crop(scroll_x, scroll_x + width)
+
+        # self._line_cache[key] = line
+        return line
